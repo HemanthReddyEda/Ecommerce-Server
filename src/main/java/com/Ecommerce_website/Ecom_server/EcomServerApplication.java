1ac85418a07255ae1e39ece2e494ea0bdf4d14c6
ehred <ehred@HEMANTHREDDY>
2024-07-31 15:04:56 +0530

    Initial commit

---
+++ src/main/java/com/Ecommerce_website/Ecom_server/EcomServerApplication.java
@@ -0,0 +1,15 @@
+package com.Ecommerce_website.Ecom_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EcomServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomServerApplication.class, args);
+	}
+
+}
